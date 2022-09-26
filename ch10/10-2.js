fc6f05102f51bb1a13d719dcd8dbd07f16033eaf
emewjin <emewjin@gmail.com>
2022-09-26 23:26:26 +0900

    10.2

--- ch10/10-2.js
+++ ch10/10-2.js
@@ -1,6 +1,19 @@
 function disabilityAmount(employee) {
+  // 💩 같은 결과인데 조건이 나뉘어져 있음. 통합하자
   if (employee.seniority < 2) return 0;
   if (employee.monthsDisabled > 12) return 0;
   if (employee.isPartTime) return 0;
+  return 1;
+}
+
+function disabilityAmount(employee) {
+  // Q. isNotEligibleForDisability 를 조회하는 역할도 맡고 있는 것이 문제가 되어 이 부분만 질의 함수로 추출하는 건가?
+  const isNotEligibleForDisability =
+    employee.seniority < 2 ||
+    employee.monthsDisabled > 12 ||
+    employee.isPartTime;
+
+  if (isNotEligibleForDisability) return 0;
+
   return 1;
 }
